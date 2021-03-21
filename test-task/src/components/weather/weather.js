9a99ea6c0c78ab9575cc8b280e3a11e7b8c0c40a
Ihor Kupa <ihorkupa@MacBook-Air-Ihor-Kupa.local>
2021-03-21 21:14:18 +0200

    Build weather component
    * Реализована функция вывода погоды в запрашиваемом городе (multilingual)
    * CSS стилизация вывода
    * Добавлен фон компонента weather

--- test-task/src/components/weather/weather.js
+++ test-task/src/components/weather/weather.js
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./weather.css";
 
-export default class Weather extends React.Component {
-  render() {
-    return (
-      <div className="weather">
-        <h1>Weather</h1>
+const api = {
+  key: "04813fad06be7cb407d737ee29d61011",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function Weather() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState("");
+
+  const search = (event) => {
+    if (event.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+        });
+    }
+  };
+
+  return (
+    <div className="weather">
+      <div className="weather-wrapper">
+        <main>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Find the weather in the city"
+              onChange={(e) => {
+                setQuery(e.target.value);
+              }}
+              value={query}
+              onKeyPress={search}
+            />
+          </div>
+          {/* Если погода не запрашивалась, выводим "" */}
+          {typeof weather.main != "undefined" ? (
+            <div>
+              <div className="location-box">
+                <div className="location">
+                  {weather.name}, {weather.sys.country}
+                </div>
+              </div>
+              <div className="weather-box">
+                <div className="temperature">
+                  {Math.round(weather.main.temp)}°c
+                </div>
+                <div className="weather">{weather.weather[0].main}</div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </main>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Weather;
