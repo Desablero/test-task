fba8e58006449cf5931e4ca21287f7e14ba026c1
Ihor Kupa <ihorkupa@MacBook-Air-Ihor-Kupa.local>
2021-03-23 04:53:27 +0200

    Customize Profile

--- test-task/src/components/profile/profile.js
+++ test-task/src/components/profile/profile.js
@@ -5,7 +5,11 @@ export default class Profile extends React.Component {
   render() {
     return (
       <div className="profile">
-        <h1>Profile</h1>
+        <div className="profile__wrapper">
+          <div>
+            <h1>¯\_(ツ)_/¯</h1>
+          </div>
+        </div>
       </div>
     );
   }
